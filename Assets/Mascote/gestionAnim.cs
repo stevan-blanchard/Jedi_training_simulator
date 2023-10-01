d00f06b1b4c96f537c5d6ef5c661dc270c11482b
stevan-blanchard <stevan.blanchard@gmail.com>
2023-10-01 22:51:11 +0200

    mark sound

--- Assets/Mascote/gestionAnim.cs
+++ Assets/Mascote/gestionAnim.cs
@@ -1,11 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class gestionAnim : MonoBehaviour
 {
     public Animator animator;
+    private AudioSource source;
+    public AudioClip sound_tir;
+    public AudioClip sound_mov;
+    public AudioClip sound_deploy;
+    public AudioClip sound_close;
+
+    public void Start()
+    {
+        source = gameObject.AddComponent<AudioSource>();
+        source.spatialBlend = 1;
+    }
+
     public void Ready(bool state) {
         try
         {
@@ -15,5 +28,27 @@ public class gestionAnim : MonoBehaviour
         {
             Debug.Log(e.ToString());
         }
+    }
+
+    public void Tir_sound() { 
+
+        source.PlayOneShot(sound_tir);
+
+    }
+
+    public void Deploy_sound()
+    {
+        Debug.Log("DEPLOIEMENT");
+        source.PlayOneShot(sound_deploy);
+    }
+
+    public void Mov_sound()
+    {
+        source.PlayOneShot(sound_mov);
+    }
+
+    public void Close_sound()
+    {
+        source.PlayOneShot(sound_close);
     }
 }
